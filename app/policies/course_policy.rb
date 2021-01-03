4a3a82c37d271aaa4ddba2b631cd1b264516be96
issarakoat <weeravut.issarakoat@gmail.com>
2021-01-03 14:00:11 -0700

    78 fixed

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,11 +6,11 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def edit?
-    @user.has_role?(:admin) || @record.user_id == @user.id
+    @record.user == @user
   end
 
   def update?
-    @user.has_role?(:admin) || @record.user_id == @user.id
+    @record.user == @user
   end
 
   def new?
