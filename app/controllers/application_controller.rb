7a189e9a6180d24965c0a9f32dabcd70848fd242
issarakoat <weeravut.issarakoat@gmail.com>
2021-01-03 22:55:11 -0700

    82 is user online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     protect_from_forgery
+    after_action :user_activity
 
     include Pundit
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -12,6 +13,11 @@ class ApplicationController < ActionController::Base
     end
 
     private
+    
+    def user_activity
+      current_user.try :touch
+    end
+  
 
     def user_not_authorized #pundit
       flash[:alert] = "You are not authorized to perform this action."
