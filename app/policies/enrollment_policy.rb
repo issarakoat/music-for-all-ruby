8cb4a6971b0684e65bbe499cb8e43c38ef6bdfc3
issarakoat <weeravut.issarakoat@gmail.com>
2021-01-10 21:32:45 -0700

    130 gem pundit for Enrollments

--- app/policies/enrollment_policy.rb
+++ app/policies/enrollment_policy.rb
@@ -1,19 +1,22 @@
-class UserPolicy < ApplicationPolicy
+class EnrollmentPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all
     end
   end
-
   def index?
     @user.has_role?(:admin)
   end
-  
+
   def edit?
-    @user.has_role?(:admin)
+    @record.user_id == @user.id
   end
 
   def update?
+    @record.user_id == @user.id
+  end
+
+  def destroy?
     @user.has_role?(:admin)
   end
 end
