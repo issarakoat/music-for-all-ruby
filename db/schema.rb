9071fd555b845c42205601453f5388961bb66e92
issarakoat <weeravut.issarakoat@gmail.com>
2021-01-15 10:35:24 -0700

    150 Calculate rating for courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_052204) do
+ActiveRecord::Schema.define(version: 2021_01_15_173038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_052204) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "slug"
+    t.float "average_rating", default: 0.0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
