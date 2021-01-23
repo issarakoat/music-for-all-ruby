85961372f7818f563060c2c01e250ecb1bc10087
issarakoat <weeravut.issarakoat@gmail.com>
2021-01-22 20:48:34 -0700

    156 styles and stuff

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,32 +1,40 @@
 module CoursesHelper
-    def enrollment_button(course)
-        if current_user
-          if course.user == current_user
-            link_to "You created this course. View analytics", course_path(course)
-          elsif course.enrollments.where(user: current_user).any?
-            link_to "You bought this course. Keep learning", course_path(course)
-          elsif course.price > 0
-            link_to number_to_currency(course.price), new_course_enrollment_path(course), class: 'btn btn-success'
-          else
-            link_to "Free", new_course_enrollment_path(course), class: 'btn btn-success'
-          end
-          #logic to buy
-        else
-          link_to "Check price", course_path(course), class: "btn btn-md btn-success"
-        end
+  def enrollment_button(course)
+    if current_user
+      if course.user == current_user
+        link_to "You created this course. View analytics", course_path(course)
+      elsif course.enrollments.where(user: current_user).any?
+        link_to "You bought this course. Keep learning", course_path(course)
+      elsif course.price > 0
+        link_to number_to_currency(course.price), new_course_enrollment_path(course), class: 'btn btn-success'
+      else
+        link_to "Free", new_course_enrollment_path(course), class: 'btn btn-success'
       end
+      #logic to buy
+    else
+      link_to "Check price", course_path(course), class: "btn btn-md btn-success"
+    end
+  end
 
-      def review_button(course)
-        user_course = course.enrollments.where(user: current_user)
-        if current_user
-          if user_course.any?
-            if user_course.pending_review.any?
-              link_to 'Add a review', edit_enrollment_path(user_course.first)
-            else
-              link_to 'Thanks for reviewing! Your Review', enrollment_path(user_course.first)
-            end
+  def review_button(course)
+    user_course = course.enrollments.where(user: current_user)
+    if current_user
+      if user_course.any?
+        if user_course.pending_review.any?
+          link_to edit_enrollment_path(user_course.first) do
+            "<i class='text-warning fa fa-star'></i>".html_safe + " " +
+            "<i class='text-dark fa fa-question'></i>".html_safe + " " +
+            'Add a review'
+          end
+        else
+          link_to enrollment_path(user_course.first) do
+            "<i class='text-warning fa fa-star'></i>".html_safe + " " +
+            "<i class='fa fa-check'></i>".html_safe + " " +
+            'Thanks for reviewing! Your Review'
           end
         end
       end
-    
+    end
+  end
+
 end
