a531bfb461eb0c61eb92ee92c7d07179fe5c32bd
issarakoat <weeravut.issarakoat@gmail.com>
2021-02-01 22:50:14 -0700

    205 link to previous and next lesson in lesson show

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -14,10 +14,19 @@ class Lesson < ApplicationRecord
 
   include RankedModel
   ranks :row_order, :with_same => :course_id
-  
+
   def to_s
     title
   end
+
+  def prev
+    course.lessons.where("row_order < ?", row_order).order(:row_order).last
+  end
+
+  def next
+    course.lessons.where("row_order > ?", row_order).order(:row_order).first
+  end
+
   def viewed(user)
     self.user_lessons.where(user: user).present?
     #self.user_lessons.where(user_id: [user.id], lesson_id: [self.id]).empty?
